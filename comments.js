75e91e1c065abd7bde429099a6f3fbdb74b58547
Osama Malik <100902550+Osmi-Bytes@users.noreply.github.com>
2024-05-22 19:57:23 +0000

    Copilot third commit

--- comments.js
+++ comments.js
@@ -1,27 +1,60 @@
 // create web server 
-var http = require('http');
-var fs = require('fs');
-var url = require('url');
-var qs = require('querystring');
+// create route for POST /comments
+// create route for GET /comments
+// create route for GET /comments/:commentId
+// create route for PUT /comments/:commentId
+// create route for DELETE /comments/:commentId
 
-// create web server
-http.createServer(function(request, response) {
-    if (request.method == 'GET') {
-        fs.readFile('comments.html', function(error, data) {
-            response.writeHead(200, { 'Content-Type': 'text/html' });
-            response.end(data);
-        });
-    } else if (request.method == 'POST') {
-        request.on('data', function(data) {
-            var query = qs.parse(data.toString());
-            var comment = query.comment;
-            console.log(comment);
-            fs.appendFile('comment.txt', comment + '\n', 'utf8', function(error) {
-                response.writeHead(302, { 'Location': '?' });
-                response.end();
-            });
-        });
-    }
-}).listen(52273, function() {
-    console.log('Server Running at https:// ');
-}   );
+const express = require('express');
+const bodyParser = require('body-parser');
+const app = express();
+const _ = require('lodash');
+const uuid = require('uuid/v4');
+
+app.use(bodyParser.json());
+
+const comments = [];
+
+app.post('/comments', (req, res) => {
+  const comment = req.body;
+  comment.id = uuid();
+  comments.push(comment);
+  res.status(201).send(comment);
+});
+
+app.get('/comments', (req, res) => {
+  res.status(200).send(comments);
+});
+
+app.get('/comments/:commentId', (req, res) => {
+  const comment = comments.find(c => c.id === req.params.commentId);
+  if (!comment) {
+    res.status(404).send();
+    return;
+  }
+  res.status(200).send(comment);
+});
+
+app.put('/comments/:commentId', (req, res) => {
+  const comment = comments.find(c => c.id === req.params.commentId);
+  if (!comment) {
+    res.status(404).send();
+    return;
+  }
+  _.merge(comment, req.body);
+  res.status(200).send(comment);
+});
+
+app.delete('/comments/:commentId', (req, res) => {
+  const index = comments.findIndex(c => c.id === req.params.commentId);
+  if (index === -1) {
+    res.status(404).send();
+    return;
+  }
+  comments.splice(index, 1);
+  res.status(204).send();
+});
+
+app.listen(3000, () => {
+  console.log('Server is listening on port 3000');
+});
