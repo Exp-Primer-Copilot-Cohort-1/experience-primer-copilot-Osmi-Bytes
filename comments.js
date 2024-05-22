2ece7cd46c36b57b05b1dacc5756968567ef2cfe
Osama Malik <100902550+Osmi-Bytes@users.noreply.github.com>
2024-05-22 20:07:43 +0000

    Copilot third commit

--- comments.js
+++ comments.js
@@ -1,60 +1,20 @@
-// create web server 
-// create route for POST /comments
-// create route for GET /comments
-// create route for GET /comments/:commentId
-// create route for PUT /comments/:commentId
-// create route for DELETE /comments/:commentId
+// Create web server
+var http = require('http');
+var fs = require('fs');
+var url = require('url');
 
-const express = require('express');
-const bodyParser = require('body-parser');
-const app = express();
-const _ = require('lodash');
-const uuid = require('uuid/v4');
+var server = http.createServer(function(request, response){
+  var urlParsed = url.parse(request.url, true);
 
-app.use(bodyParser.json());
-
-const comments = [];
-
-app.post('/comments', (req, res) => {
-  const comment = req.body;
-  comment.id = uuid();
-  comments.push(comment);
-  res.status(201).send(comment);
-});
-
-app.get('/comments', (req, res) => {
-  res.status(200).send(comments);
-});
-
-app.get('/comments/:commentId', (req, res) => {
-  const comment = comments.find(c => c.id === req.params.commentId);
-  if (!comment) {
-    res.status(404).send();
-    return;
+  if(urlParsed.pathname == '/comment'){
+    response.writeHead(200, {'Content-Type': 'text/html'});
+    response.end('Comment posted: ' + urlParsed.query.comment);
+  } else {
+    response.writeHead(404, {'Content-Type': 'text/html'});
+    response.end('404: Page not found');
   }
-  res.status(200).send(comment);
 });
 
-app.put('/comments/:commentId', (req, res) => {
-  const comment = comments.find(c => c.id === req.params.commentId);
-  if (!comment) {
-    res.status(404).send();
-    return;
-  }
-  _.merge(comment, req.body);
-  res.status(200).send(comment);
-});
-
-app.delete('/comments/:commentId', (req, res) => {
-  const index = comments.findIndex(c => c.id === req.params.commentId);
-  if (index === -1) {
-    res.status(404).send();
-    return;
-  }
-  comments.splice(index, 1);
-  res.status(204).send();
-});
-
-app.listen(3000, () => {
+server.listen(3000, function(){
   console.log('Server is listening on port 3000');
 });
